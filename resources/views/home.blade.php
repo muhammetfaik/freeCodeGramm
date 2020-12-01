861ebf743b7ff872ba54cd6cd8b439f758573a45
Muhammet Faik Alada <muhammetfaik@gmail.com>
2020-12-01 16:00:57 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/resources/img/icon_large.png" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong>posts</div>
+                <div class="pr-5"><strong>23k</strong>posts</div>
+                <div class="pr-5"><strong>212</strong>posts</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
